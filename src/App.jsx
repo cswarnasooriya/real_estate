fa746bd9c61a2edec8f72077ba627fa2fe0f1a1f
Chanuka Sandaruwan Warnasooriya <warnasooriyacs1000@gmail.com>
2023-12-19 10:58:01 +0530

    Navbar updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
 import Navbar from './Components/Navbar/Navbar';
-import Footer from './Components/Footer/Footer';
-import Hello from './Components/Hello/Hello';
-import Contact from './Components/Contact/Contact';
-import Listing from './Components/Listing/Listing';
-import About from './Components/About/About';
-import Popular from './Components/Popular/Popular';
-import Sponser from './Components/Sponser/Sponser';
-import Subscribe from './Components/Subscribe/Subscribe';
+// import Footer from './Components/Footer/Footer';
+// import Hello from './Components/Hello/Hello';
+// import Contact from './Components/Contact/Contact';
+// import Listing from './Components/Listing/Listing';
+// import About from './Components/About/About';
+// import Popular from './Components/Popular/Popular';
+// import Sponser from './Components/Sponser/Sponser';
+// import Subscribe from './Components/Subscribe/Subscribe';
 
 
 
@@ -17,14 +17,7 @@ function App() {
   return (
     <>
       <Navbar/>
-      <Footer/>
-      <Hello/>
-      <Contact/>
-      <Listing/>
-      <About/>
-      <Popular/>
-      <Sponser/>
-      <Subscribe/>
+     
 
     </>
   )
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./navbar.css"
+import { SiHomeassistant } from "react-icons/si";
+import { IoIosCloseCircleOutline } from "react-icons/io";
+import { TbGridDots } from "react-icons/tb";
+
+
 
 const Navbar = () => {
+  
+  const [menu, setMenu] = useState('menu')
+  const shownavbar = ()=>{
+    setMenu("shownavbar menu");
+  };
+
+  const removenavbar = ()=>{
+    setMenu("menu");
+  };
+
+  //function to show navbar
   return (
-    <div>
-      Navbar
+    <div className='navbar'>
+      <div className="logoDiv">
+        <SiHomeassistant className="icon"/>
+        <span>Dremz</span>
+      </div>  
+      
+
+      <div className={menu}>
+        <ul>
+          <li className="navList">Property</li>
+          <li className="navList">Services</li>
+          <li className="navList">Product</li>
+          <li className="navList">About Us</li>
+
+        </ul>
+        {/*icons to close navbar on small devices*/}
+        <IoIosCloseCircleOutline className='icon closeIcon' onClick={removenavbar}/>
+      </div>
+
+
+
+      <button className='contactBtn btn'>Contact</button>
+      {/*icons to open/show navbar on small devices*/}
+      <TbGridDots className='icon menuIcon' onClick={shownavbar}/>
+
+
+
+      
     </div>
   )
 }
