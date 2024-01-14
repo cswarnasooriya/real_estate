c4ac11595d1d741844eeadad51d51e2147060ec7
Chanuka Sandaruwan Warnasooriya <warnasooriyacs1000@gmail.com>
2024-01-14 11:14:27 +0530

    Popular section updated

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
+import "./popular.css"
+
+import { FaArrowLeft } from "react-icons/fa6";
+import { FaArrowRight } from "react-icons/fa6";
+
+//images
+import image1 from '../../Assets/image4.jpg';
+import image2 from '../../Assets/image3.jpg';
+import image3 from '../../Assets/image5.jpg';
+
 
 const Popular = () => {
   return (
     <div className='popular container section'>
         <div className="secContainer">
-            <div className="secHeader">
+            <div className="secHeader flex">
                 <div className="textDiv">
                     <span className='orangeText'>
                         Best Choice
@@ -16,14 +26,87 @@ const Popular = () => {
                 </div>
 
                 <div className="icons flex">
-                     
+                    <FaArrowLeft className='icon'/>
+                    <FaArrowRight className='icon'/>   
+                </div>
+            
+            </div>
+
+            <div className="secContent grid">
+
+
+            {/*House 1 */}
+                <div className="singleProperty">
+                    <div className="imgDiv">
+                        <img src={image1} alt="House Image" />
+                    </div>
+
+                    <div className="info">
+                        <span className="price">
+                            <span className="orangeText">
+                                $
+                            </span>
+                            <span className="digits">
+                                45,550
+                            </span>
+                        </span>
+                        <h2 className="name">Enjoy Prica Jarfin</h2>
+                        <p>
+                            A stunning penthouse with skyline view and private terrace.
+                        </p>
+                    </div>
                 </div>
 
-            </div>
-        </div>
-      
-    </div>
-  )
-}
 
-export default Popular
+                    {/*House 2 */}
+                <div className="singleProperty">
+                    <div className="imgDiv">
+                        <img src={image2} alt="House Image" />
+                    </div>
+
+                    <div className="info">
+                        <span className="price">
+                            <span className="orangeText">
+                                $
+                            </span>
+                            <span className="digits">
+                                52,850
+                            </span>
+                        </span>
+                        <h2 className="name">Mountain Retreat, Aspen</h2>
+                        <p>
+                            A enjoying life with skyline view and holiday terrace.
+                        </p>
+                    </div>
+                </div>
+
+                {/*House 3 */}
+                <div className="singleProperty">
+                    <div className="imgDiv">
+                        <img src={image3} alt="House Image" />
+                    </div>
+
+                    <div className="info">
+                        <span className="price">
+                            <span className="orangeText">
+                                $
+                            </span>
+                            <span className="digits">
+                                55,200
+                            </span>
+                        </span>
+                        <h2 className="name">Contemorary Conde</h2>
+                        <p>
+                            A marvel cityside and tour location in holiday benches.
+                        </p>
+                    </div>
+                </div>
+         </div>
+    
+    </div>
+      
+  </div>
+  );
+};
+
+export default Popular;
